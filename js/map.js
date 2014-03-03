f2b65d9bbffa3a40e8cf99850d5562d781d19a53
skwilkinson <skw2123@columbia.edu>
2014-03-02 19:17:18 -0500

    Work for 3/2

--- js/map.js
+++ js/map.js
@@ -6,25 +6,62 @@ var myCenter=new google.maps.LatLng(51.508742,-0.120850);
 
 function initialize()
 {
-var mapProp = {
-  center: myCenter,
-  zoom:5,
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+	var mapProp = {
+  	center: myCenter,
+  	zoom:7,
+  	mapTypeId: google.maps.MapTypeId.SATELLITE,
+  	mapTypeControl: false,
+  	streetViewControl: false,
+  	zoomControl: false,
+	panControl: false,
+  	scaleControl: true
+  	};
 
-  map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  	map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  	
+	var image = new google.maps.MarkerImage("ISS.png",
+      new google.maps.Size(21, 21),     // size
+      new google.maps.Point(0, 0),      // origin
+      new google.maps.Point(10, 10));   // anchor
+      
+  	crossHair = new google.maps.Marker({
+	    position: map.getCenter(),
+	    map: map,
+	    icon: image
+	});
 
-  marker = new google.maps.Marker({
-  position: myCenter,
-  title:'Click to zoom'
-  });
-
-marker.setMap(map);
+	crossHair.setMap(map);
 }
 
 var map;
-var marker;
+var crossHair;
 
+function updatePosition() {
+
+	$.getJSON('http://api.open-notify.org/iss-now.json?callback=?', function(data) 
+	{
+    if (data) {
+		lat = data.iss_position.latitude;
+		lon = data.iss_position.longitude;
+		ISS_loc = new google.maps.LatLng(lat,lon);
+	}
+	
+	var ISS_loc;
+	map.panTo(ISS_loc);
+    crossHair.setPosition(ISS_loc);
+  });
+
+}
+var liveUpdate = setInterval(function(){updatePosition()},1000);
+
+function startUpdate() {
+	liveUpdate = setInterval(function(){updatePosition()},1000);
+	updatePosition();
+}
+
+function stopUpdate() {
+	window.clearInterval(liveUpdate);
+}
 function goToPhilly()
 {
 	var philly = new google.maps.LatLng(39.9500,-75.1700);
