a7ba77f133d724efe6c570f3cab7975472c931d7
dsmit3000 <dsmit3000@gmail.com>
2014-03-03 20:53:30 -0500

    Update table.js

--- js/table.js
+++ js/table.js
@@ -22,6 +22,8 @@ j = i+1;
 
 x=xmlDoc.getElementsByTagName('wmc__TEOSite');
 y=xmlDoc.getElementsByTagName('TGeoCoordsEx');
+numbSites = xmlDoc.getElementsByTagName('wmc__TEOSite').length;
+
 
 addTable();
 }
@@ -32,6 +34,7 @@ if(i<5)
 {
 i = i+1;
 j = j+1;
+
 deleteTable();	
 addTable();
 }
@@ -61,25 +64,60 @@ var mytable=document.getElementById("sitesTable");
 
 var newrow=mytable.insertRow(-1); //add new row to end of table
 var newcell=newrow.insertCell(0); //insert new cell to row
-newcell.innerHTML=i+1;
+newcell.innerHTML=i+1 + " of " + numbSites;
 var newcell=newrow.insertCell(0); //insert new cell to row
 newcell.innerHTML="Site #";
 	
 var newrow=mytable.insertRow(-1); //add new row to end of table
 var newcell=newrow.insertCell(0); //insert new cell to row
 newcell.innerHTML=x[i].getAttribute('Nomenclature');
+
 var newcell=newrow.insertCell(0); //insert new cell to row
 newcell.innerHTML="Location";
 
 var newrow=mytable.insertRow(-1); //add new row to end of table
 var newcell=newrow.insertCell(0); //insert new cell to row
-newcell.innerHTML=y[i].getAttribute('lat') + ", " + y[i].getAttribute('lon');
+
+var str = x[i].getAttribute('Notes');
+var m = str.indexOf(";",0);
+var time = str.substring(0,m);
+newcell.innerHTML = time;
+
 var newcell=newrow.insertCell(0); //insert new cell to row
-newcell.innerHTML="Lat,Lon";
+newcell.innerHTML="Time";
 
 var newrow=mytable.insertRow(-1); //add new row to end of table
 var newcell=newrow.insertCell(0); //insert new cell to row
-newcell.innerHTML=x[i].getAttribute('Notes');
+
+var n = str.indexOf(";",m+1);
+var lens = str.substring(m+1,n);
+newcell.innerHTML = lens;
+
+var newcell=newrow.insertCell(0); //insert new cell to row
+newcell.innerHTML="Lens";
+
+var newrow=mytable.insertRow(-1); //add new row to end of table
+var newcell=newrow.insertCell(0); //insert new cell to row
+
+var o = str.indexOf(";",n+1);
+var notes = str.substring(n+1,o);
+newcell.innerHTML = notes;
+
 var newcell=newrow.insertCell(0); //insert new cell to row
 newcell.innerHTML="Notes";
+
+var newrow=mytable.insertRow(-1); //add new row to end of table
+var newcell=newrow.insertCell(0); //insert new cell to row
+
+var pa = str.indexOf("lat",o+1);
+var pb = str.indexOf(",",pa);
+var lat = str.substring(pa,pb);
+
+var qa = str.indexOf("lon",o+1);
+var qb = str.indexOf("at",pb);
+var lon = str.substring(qa,qb);
+newcell.innerHTML = lat +", "+ lon;
+
+var newcell=newrow.insertCell(0); //insert new cell to row
+newcell.innerHTML="Lat, Long";
 }
